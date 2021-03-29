f2132858ed6dd94ee7955e93fdbf63f30d96dd2e
Pasquale Matarrese <pasquale.matarrese@gmail.com>
2021-03-29 18:56:51 +0200

    gh-pages deplay

--- src/datamock.js
+++ src/datamock.js
@@ -1,73 +1,134 @@
 export const TURNISTI = [
   {
-    name: "Alessia",
-    id: "alessia_01",
+    name: 'Alessia',
+    id: 'alessia_01',
+    schedule: {
+      2021: {
+        4: {
+          1: 'night',
+          3: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          7: 'morning',
+          9: 'morning',
+          10: 'morning',
+          29: 'morning'
+        },
+        5: {
+          1: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          9: 'morning',
+          10: 'morning'
+        },
+        6: {
+          1: 'night',
+          3: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          7: 'morning',
+          9: 'morning',
+          10: 'morning',
+          29: 'morning'
+        },
+        7: {
+          1: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          9: 'morning',
+          10: 'morning'
+        },
+        8: {
+          1: 'night',
+          3: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          7: 'morning',
+          9: 'morning',
+          10: 'morning',
+          29: 'morning'
+        },
+        9: {
+          1: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          9: 'morning',
+          10: 'morning'
+        },
+        10: {
+          1: 'night',
+          3: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          7: 'morning',
+          9: 'morning',
+          10: 'morning',
+          29: 'morning'
+        },
+        11: {
+          1: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          9: 'morning',
+          10: 'morning'
+        },
+        12: {
+          1: 'night',
+          3: 'morning',
+          4: 'afternoon',
+          6: 'night',
+          7: 'morning',
+          9: 'morning',
+          10: 'morning',
+          29: 'morning'
+        }
+      }
+    }
+  },
+  {
+    name: 'Melissa',
+    id: 'melissa_01',
     schedule: {
       2021: {
         1: {
-          1: "night",
-          3: "morning",
-          4: "afternoon",
-          6: "night",
-          7: "morning",
-          9: "morning",
-          10: "morning",
-          29: "morning",
-        },
-        2: {
-          1: "morning",
-          4: "afternoon",
-          6: "night",
-          9: "morning",
-          10: "morning",
-        },
-      },
-    },
+          1: 'afternoon',
+          5: 'morning',
+          6: 'morning',
+          8: 'afternoon',
+          10: 'night'
+        }
+      }
+    }
   },
   {
-    name: "Melissa",
-    id: "melissa_01",
+    name: 'Martina',
+    id: 'martina_08',
     schedule: {
       2021: {
         1: {
-          1: "afternoon",
-          5: "morning",
-          6: "morning",
-          8: "afternoon",
-          10: "night",
-        },
-      },
-    },
+          1: 'morning',
+          3: 'night',
+          4: 'morning',
+          7: 'night',
+          9: 'afternoon',
+          12: 'night'
+        }
+      }
+    }
   },
   {
-    name: "Martina",
-    id: "martina_08",
-    schedule: {
-      2021: {
-        1: {
-          1: "morning",
-          3: "night",
-          4: "morning",
-          7: "night",
-          9: "afternoon",
-          12: "night",
-        },
-      },
-    },
+    name: 'Maria',
+    id: 'maria_04',
+    schedule: {}
   },
   {
-    name: "Maria",
-    id: "maria_04",
-    schedule: {},
+    name: 'Giovanni',
+    id: 'giovanni_01',
+    schedule: {}
   },
   {
-    name: "Giovanni",
-    id: "giovanni_01",
-    schedule: {},
-  },
-  {
-    name: "Ciccio",
-    id: "ciccio_23",
-    schedule: {},
-  },
+    name: 'Ciccio',
+    id: 'ciccio_23',
+    schedule: {}
+  }
 ];
